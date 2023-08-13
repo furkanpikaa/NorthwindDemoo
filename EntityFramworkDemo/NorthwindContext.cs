d683dedf2def432c0331f93542ed4dd5c6942113
Pikaa <furkansrds@gmail.com>
2023-08-13 16:08:21 +0300

    Add project files.

---
+++ EntityFramworkDemo/NorthwindContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=master;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //fluent mapping
+            //modelBuilder.HasDefaultSchema("Admin");
+            modelBuilder.Entity<Personel>().ToTable("Employees");
+
+            modelBuilder.Entity<Personel>().Property(p => p.Id).HasColumnName("EmployeeId");
+            modelBuilder.Entity<Personel>().Property(p => p.Name).HasColumnName("FirstName");
+            modelBuilder.Entity<Personel>().Property(p => p.Surname).HasColumnName("LastName");
+
+        }
+
+    }
+    
+    
+}
---
+++ EntityFramworkDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace NorthwindDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sql ve ilişkisel Veritabanı
+            //ADO.NET
+            //Entity Framework - Bir ORM - Objcet relational mapping
+            //NHibernate 
+            //Dapper
+
+            GetAll();
+            //GetProductsByCategory(1);
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            //foreach (var product in northwindContext.Products)
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var personel in northwindContext.Personels)
+            {
+                Console.WriteLine("{0} / {1} / {2}", personel.Id,personel.Name,personel.Surname);
+            }
+        }
+
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            var result = northwindContext.Products.Where(p=>p.CategoryId == categoryId);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+    }
+}
